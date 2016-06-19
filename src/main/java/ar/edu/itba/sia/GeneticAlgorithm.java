c5616ca049510968e210880edf53804de53c2387
ioji <ioji@chervonagura.com.ar>
2016-06-18 23:02:44 -0300

    Re calculate fitness

--- src/main/java/ar/edu/itba/sia/GeneticAlgorithm.java
+++ src/main/java/ar/edu/itba/sia/GeneticAlgorithm.java
@@ -5,6 +5,7 @@ import ar.edu.itba.sia.replacement.MethodThreeReplacement;
 import ar.edu.itba.sia.replacement.Replacement;
 import ar.edu.itba.sia.selection.Roulette;
 import ar.edu.itba.sia.selection.Universal;
+import ar.edu.itba.sia.util.FitnessCalculator;
 import ar.edu.itba.sia.util.WarriorsGenerator;
 import ar.edu.itba.sia.model.Warrior;
 import ar.edu.itba.sia.mutation.Mutation;
@@ -26,14 +27,15 @@ public class GeneticAlgorithm {
         }
         System.out.println();
 
-        Selection selection = new Elitism();
+        Selection selection = new Universal();
         Crossover crossover = new AnnularCrossover(0.8);
         Mutation mutation = new NonUniformMutation(0.005);
-        Selection rep = new Elitism();
+        Selection rep = new Roulette();
         Replacement replacement = new MethodThreeReplacement();
 
         int i = 0;
         while (i++ < 100) { //TODO implement a better break condition
+            FitnessCalculator.calculateTotalFitness(warriors);
             warriors = replacement.replace(20, warriors, selection, crossover, mutation, rep);
         }
 
