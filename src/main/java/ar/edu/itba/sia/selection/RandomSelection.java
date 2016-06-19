73b9b193b512dd3b49116933311a540fe11d6ec2
ioji <ioji@chervonagura.com.ar>
2016-06-19 20:27:46 -0300

    Fix random selection

--- src/main/java/ar/edu/itba/sia/selection/RandomSelection.java
+++ src/main/java/ar/edu/itba/sia/selection/RandomSelection.java
@@ -3,6 +3,7 @@ package ar.edu.itba.sia.selection;
 import ar.edu.itba.sia.model.Warrior;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class RandomSelection implements Selection {
@@ -10,6 +11,8 @@ public class RandomSelection implements Selection {
     @Override
     public List<Warrior> select(int amount, List<Warrior> population) {
         Collections.shuffle(population);
-        return population.subList(0, amount);
+        List<Warrior> selected = new LinkedList<>();
+        selected.addAll(population.subList(0, amount));
+        return selected;
     }
 }
